7f0e0f7d32f7261f5ac1283de3117407d0ffe801
Alex <alexandersuglio@gmail.com>
2017-03-15 23:53:46 -0400

    Here it is

---
+++ crystal_game_hw.js
@@ -0,0 +1,96 @@
+// $("body").click(function() {
+//     alert('hey');
+// });
+
+//create a random math to for answer value
+//create four difference random maths for the different pictures 
+//assign values to the pictures
+
+//create working win loss counters that correspond to results above
+//if result == target win++
+// if != target loss++
+
+// $("#value").click(function() {
+//     for (i = 0; i < 50; i++) {
+
+//         // computerResult = Math.floor(Math.random() * computerLetterGuess.length + 1),
+
+//         var targetResult = Math.floor(Math.random() * 50 + 1);
+//         console.log(targetResult);
+
+
+//     };
+// });
+
+function createTarget() {
+var targetResult = Math.floor(Math.random() * 50 + 1);
+console.log(targetResult);
+};
+
+createTarget();
+
+
+
+
+
+
+
+// $("#pic1").click(function() {
+//   // for (i = 0; i < 50; i++) {
+
+
+
+
+//     // };
+// });
+
+
+
+
+// $("#pic2").click(function() {
+// 	 alert('hey');
+//     // for (i = 0; i < 50; i++) {
+
+//     //     // computerResult = Math.floor(Math.random() * computerLetterGuess.length + 1),
+
+//     //     var targetResult = Math.floor(Math.random() * 50 + 1);
+//     //     console.log(targetResult);
+
+
+//     // };
+// });
+
+// $("#pic3").click(function() {
+// 	 alert('hey');
+//     // for (i = 0; i < 50; i++) {
+
+//     //     // computerResult = Math.floor(Math.random() * computerLetterGuess.length + 1),
+
+//     //     var targetResult = Math.floor(Math.random() * 50 + 1);
+//     //     console.log(targetResult);
+
+
+//     // };
+// });
+
+// $("#pic4").click(function() {
+// 	 alert('hey');
+//     // for (i = 0; i < 50; i++) {
+
+//     //     // computerResult = Math.floor(Math.random() * computerLetterGuess.length + 1),
+
+//     //     var targetResult = Math.floor(Math.random() * 50 + 1);
+//     //     console.log(targetResult);
+
+
+//     // };
+// });
+
+
+
+// // function targetCreator() {
+
+// //     };
+// // };
+
+// // targetCreator();
